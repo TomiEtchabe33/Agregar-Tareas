8ab264ea06632b13b7dc3d1aab663f233267e007
TomiEtchabe33 <etchabetomas33@gmail.com>
2023-04-06 12:42:51 -0300

    agregartareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+    const newTodo = prompt('Colocar nueva tarea:');
+    setTodos([...todos, { task: newTodo, completed: false }]);
+  }
+
+  function completeTask(index) {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = true;
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h1>Tareas</h1>
+      <button className='boton boton1' onClick={addTodo}>Agregar tarea</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <p style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.task} <button className='botoncompletar botoncompletar1' onClick={() => completeTask(index)}>Completar</button>
+          </p>
+        ))}
+      </ul>
+      </center>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
